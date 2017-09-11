ecd3f33babcf2d1a4eacf1ab11ed7f5db52c58e1
Marvin Altemeier <marv.internetz@gmail.com>
2017-09-11 20:17:15 +0200

    Changed project description

--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,14 @@
-Readme wird später ausführlicher.
+# Jodel-Parse
 
-Wichtigste Kommandos sind `yarn build` um die Typescript source zu transpilieren und `yarn scrape` um die `index.html` in die docs zu generieren.
+## Install
 
-Ansonsten schaut bei Pull-Requests dass alles durch den Linter geht, hab noch keine Hooks eingerichtet.
+To install the tool, run either `yarn` or `npm install` depending on what you are using.
+
+## How to use
+
+To scrape the data of a jodel, navigate to its web-presentation (by using the share link), get into the source of the page and and copy all `li`-elements.
+Place these into the `questions.txt` and run `yarn scrape`, which will output the parsed questions to `./docs/index.html`.
+
+## Contribution
+
+Feel free to contribute, Merge requests which do not pass TSLint won't be accepted.
--- src/scrape.ts
+++ src/scrape.ts
@@ -41,26 +41,12 @@ function scrapeDocument(err: NodeJS.ErrnoException, data: Buffer): void {
 <!doctype html>
 <html>
     <head>
-        <title>Lindner-Jodel AMA ~ Zusammenfassung</title>
+        <title>Parsed Jodel-Content</title>
     </head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     <body>
-    <a href="https://github.com/iMarv/Jodel-Lindner"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/652c5b9acfaddf3a9c326fa6bde407b87f7be0f4/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png"></a>
-        <div class="jumbotron">
-            <h1 class="display-3">Christian Lindner - Jodel AMA</h1>
-            <hr class="my-4">
-            <p>Hi! Ich bin Marvin und habe versucht das Jodel-AMA von Christian Lindner im Nachhinein zu lesen - relativ erfolglos weil es ein riesiges Durcheinander ist.
-            Daher habe ich ein Tool entwickelt, welches versucht weitestgehend die passenden Fragen zu Christian Lindners Antworten zu suchen.
-            Wenn mehrere Jodel des selben Nutzers in Frage kommen, so seht ihr unten alle aufgelistet und müsst entsprechend euren gesunden Menschenverstand nutzen um herauszufinden welche denn die passende Frage ist.</p>
-            <div class="card text-white bg-danger mb-6">
-                <div class="card-body">
-                    Das Design der Seite ist noch nicht fertig und meiner Müdigkeit geschuldet, der Programmcode der die Antworten sucht wird auch verbesserst sobald ich am Mittwoch von der Arbeit komme.<br />
-                    Ihr könnt also entweder auf meinen Feierabend morgen warten, oder wenn ihr technisch was drauf habt, das Repository dieser Seite auf GitHub forken und das Design anpassen,<br /> ich werde versuchen in meiner Mittagspause eventuelle Pull-Requests zu bearbeiten.
-                </div>
-            </div>
-        </div>
         <div class="container">
-            ${ toHtml(pairs.slice(0, pairs.length - 1)) /* Hotfix because I am tired and magically CLs last answer is intepreted as a question */}
+            ${ toHtml(pairs.slice(0, pairs.length - 1)) /* Hotfix because I am tired and magically OJs last answer is intepreted as a question */}
         </div>
     </body>
 </html>`;
@@ -92,7 +78,7 @@ function toHtml(pairs: QuestionAnswerPair[]): string {
     <div class="col-md-6">
         ${ pair.questions.map(question => `
         <div class="card">
-            <div class="card-header">Frage von @${ question.userNumber }</div>
+            <div class="card-header">Question from @${ question.userNumber }</div>
             <div class="card-body">
                 <p class="card-text">${ question.text }</p>
             </div>
@@ -100,7 +86,7 @@ function toHtml(pairs: QuestionAnswerPair[]): string {
     </div>
     <div class="col-md-6">
         <div class="card bg-secondary">
-            <div class="card-header">Antwort von Christian Lindner</div>
+            <div class="card-header">OJs Answer</div>
             <div class="card-body">
                 <p class="card-text">${ pair.answer.text }</p>
             </div>
